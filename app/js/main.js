797c1daa743707ffa4d65554a0d66470641787dd
Yurii Trihub <yuriitrihub@gmail.com>
2021-08-28 20:04:25 +0300

    вёрстка и стилизация галереи страницы contact, завершение стилизации страницы contact

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,14 @@ $(function () {
       fade: true,
       autoplay: true
    });
+
+   $('.contact-slider').slick({
+      slidesToShow: 10,
+      slidesToScroll: 10,
+      dots: true,
+      arrows: false,
+   });
+
    var mixer = mixitup('.gallery__inner', {
       load: {
          filter: '.living'
